86677e716bc0915cc83c187d1b1b5a611135603e
Shrirang Mohare <166914392+mute-x-satcher@users.noreply.github.com>
2026-04-26 11:57:56 +0530

    Update baseurl.js -- fixing Cloudflare Tunnel due to Ping and CGNAT

--- url/baseurl.js
+++ url/baseurl.js
@@ -1,5 +1,5 @@
 const LOCAL_URL = 'http://localhost:3001';
 const REMOTE_URL = 'https://sps-backend-eryv.onrender.com'
-const CLOUDFLARE_URL = 'https://sporting-beverly-accounts-transportation.trycloudflare.com'
+const CLOUDFLARE_URL = 'https://installing-sound-boots-software.trycloudflare.com'
 const BASE_URL = CLOUDFLARE_URL;
 export default BASE_URL
