510e04c3e8e21474b639d989350885795f321802
Shrirang Mohare <mutex7171@gmail.com>
2026-05-02 09:34:59 +0530

    Fixed taskDate Err --

--- url/baseurl.js
+++ url/baseurl.js
@@ -1,5 +1,5 @@
 const LOCAL_URL = 'http://localhost:3001';
 const REMOTE_URL = 'https://sps-backend-eryv.onrender.com'
 const CLOUDFLARE_URL = 'https://installing-sound-boots-software.trycloudflare.com'
-const BASE_URL = LOCAL_URL;
+const BASE_URL = REMOTE_URL;
 export default BASE_URL
