65d6c891088622d4c10eac167766240c13ab8c9e
Shrirang Mohare <mutex7171@gmail.com>
2026-05-03 12:17:35 +0530

    Added a api.lipix.online

--- url/baseurl.js
+++ url/baseurl.js
@@ -1,5 +1,5 @@
 const LOCAL_URL = 'http://localhost:3001';
 const REMOTE_URL = 'https://sps-backend-eryv.onrender.com'
-const CLOUDFLARE_URL = 'https://installing-sound-boots-software.trycloudflare.com'
+const CLOUDFLARE_URL = 'https://api.lipix.online'
 const BASE_URL = REMOTE_URL;
 export default BASE_URL
