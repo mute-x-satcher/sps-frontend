c72f557e1fb8a9e22af68619f46e542c99d44e72
Shrirang Mohare <mutex7171@gmail.com>
2026-05-03 21:56:39 +0530

    test-api --

--- url/baseurl.js
+++ url/baseurl.js
@@ -1,5 +1,5 @@
 const LOCAL_URL = 'http://localhost:3001';
 const REMOTE_URL = 'https://sps-backend-eryv.onrender.com'
-const CLOUDFLARE_URL = 'https://api.lipix.online'
+const CLOUDFLARE_URL = 'https://test-api.lipix.online'
 const BASE_URL = CLOUDFLARE_URL;
 export default BASE_URL
